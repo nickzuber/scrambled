8df18059096a0a8ce8a52d930b9108462924c79d
Nicholas Zuber <zuber.nicholas@gmail.com>
2024-08-11 20:19:58 -0400

    meta

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -149,9 +149,9 @@ export const Controls: FC = () => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
               d="M15.25 10.75L12 14.25L8.75 10.75"
             ></path>
           </svg>
